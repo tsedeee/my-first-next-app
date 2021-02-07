f61eab01781f18aee293dde88ecc7d5f4205fb58
unknown <tseegii9512@gmail.com>
2021-02-07 23:29:32 +0800

    гарчиг дээр өөрчилсөн

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Миний NextJS сайт</title>
+        <title>Миний NextJS сайт!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
